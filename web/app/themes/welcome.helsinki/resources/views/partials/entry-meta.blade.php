b8d6fc11e5ff8aa94e81f0630b0635beb8ba1787
Henri Tukiainen <henri@momocode.com>
2021-01-14 01:10:28 +0200

    Add tags to article sidebar

--- web/app/themes/welcome.helsinki/resources/views/partials/entry-meta.blade.php
+++ web/app/themes/welcome.helsinki/resources/views/partials/entry-meta.blade.php
@@ -3,6 +3,16 @@
     {{ sprintf(__('Updated %s', 'hds'), get_the_date()) }}
   </time>
 
+  @if (get_the_tags())
+  <ul class="tags">
+    @foreach (get_the_tags() as $tag)
+      <li>
+        <a href="{!! get_tag_link($tag) !!}">{!! $tag->name !!}</a>
+      </li>
+    @endforeach
+  </ul>
+  @endif
+
   <!-- <p class="byline author vcard">
     <span>{{ __('By', 'sage') }}</span>
     <a href="{{ get_author_posts_url(get_the_author_meta('ID')) }}" rel="author" class="fn">
