06930f49e014f5ce40c33545f608c409137689a6
Henri Tukiainen <henri@momocode.com>
2021-03-26 11:27:51 +0200

    Add Facebook and Google verification tags

--- web/app/themes/welcome.helsinki/index.php
+++ web/app/themes/welcome.helsinki/index.php
@@ -4,6 +4,8 @@
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="facebook-domain-verification" content="pk6qbeqnbylh3m3uq01kublwiiyfvg" />
+    <meta name="google-site-verification" content="sjFXkjiunQrv6RQQRBGhCjbiS55KaSPsEKlLOgwoitI" />
     <?php wp_head(); ?>
   </head>
 
