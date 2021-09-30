8eea5cbd26b71833b7f4c0f50f329ca8e1142637
Oskar Schöldström <m@oxy.fi>
2021-09-30 14:10:04 -0300

    Accessibility: Fix search input description read out while reading page content [4.2]

--- web/app/themes/welcome.helsinki/resources/views/partials/search.blade.php
+++ web/app/themes/welcome.helsinki/resources/views/partials/search.blade.php
@@ -1,8 +1,8 @@
 <form action="{{ home_url('/') }}" method="get" role="search" class="site-search">
   <input class="site-search__field" type="search" name="s" placeholder="{{ __('Search', 'welcome.helsinki') }}" data-swplive="true" />
 
-  <button class="site-search__button" type="submit">
-    @svg('images/icons/search.svg', '', ['width' => 24, 'height' => 24])
+  <button class="site-search__button" type="submit" role="button">
+    @svg('images/icons/search.svg', '', ['width' => 24, 'height' => 24, 'aria-hidden' => 'true'])
     <span class="sr-only">{{ __('Search', 'welcome.helsinki') }}</span>
   </button>
 </form>
