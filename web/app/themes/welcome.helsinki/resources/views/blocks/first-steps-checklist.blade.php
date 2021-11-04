9bd94479fd8cd0b1809eb47ef1606a22019e65d6
Henri Tukiainen <henri@momocode.com>
2021-11-04 11:56:42 +0200

    Fix first steps checklist options being corrupted in some cases

--- web/app/themes/welcome.helsinki/resources/views/blocks/first-steps-checklist.blade.php
+++ web/app/themes/welcome.helsinki/resources/views/blocks/first-steps-checklist.blade.php
@@ -8,8 +8,8 @@
           pdfBaseUrl: '{{ get_field('pdf_base_url', 'option') }}' || undefined,
           emailBaseUrl: '{{ get_field('email_base_url', 'option') }}' || undefined,
           emailQueryKey: '{{ get_field('email_query_key', 'option') }}' || undefined,
-          questions: {!! get_field('questions', 'option') !!},
-          checklist: {!! get_field('checklist', 'option') !!}
+          questions: {!! get_option('options_questions') !!},
+          checklist: {!! get_option('options_checklist') !!}
         }
       })
     } catch (err) {
